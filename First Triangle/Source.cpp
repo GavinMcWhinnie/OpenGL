536ee7c221b339693eaf815f3f304779c17c0f32
Gavin McWhinnie <25037065+GavinMcWhinnie@users.noreply.github.com>
2020-06-22 17:32:59 +0100

    Added first triangle
    Lots of copied code cause not sure exactly whats going on

---
+++ First Triangle/Source.cpp
@@ -0,0 +1,163 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <vector>
+
+GLenum glCheckError_(const char* file, int line)
+{
+    GLenum errorCode;
+    while ((errorCode = glGetError()) != GL_NO_ERROR)
+    {
+        std::string error;
+        switch (errorCode)
+        {
+        case GL_INVALID_ENUM:                  error = "INVALID_ENUM"; break;
+        case GL_INVALID_VALUE:                 error = "INVALID_VALUE"; break;
+        case GL_INVALID_OPERATION:             error = "INVALID_OPERATION"; break;
+        case GL_STACK_OVERFLOW:                error = "STACK_OVERFLOW"; break;
+        case GL_STACK_UNDERFLOW:               error = "STACK_UNDERFLOW"; break;
+        case GL_OUT_OF_MEMORY:                 error = "OUT_OF_MEMORY"; break;
+        case GL_INVALID_FRAMEBUFFER_OPERATION: error = "INVALID_FRAMEBUFFER_OPERATION"; break;
+        }
+        std::cout << error << " | " << file << " (" << line << ")" << std::endl;
+    }
+    return errorCode;
+}
+#define glCheckError() glCheckError_(__FILE__, __LINE__) 
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        return -1;
+
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+    {
+        std::cout << "uh oh" << std::endl;
+    }
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+    
+    
+    float positions[] = {
+            -0.5f, -0.5f,
+             0.5f, -0.5f, 
+             0.0f,  0.5f, 
+            -0.5f,  0.5f
+    };
+
+    unsigned int indices[] = {
+            0, 1, 2
+    };
+
+    unsigned int vertexArray;
+    glGenVertexArrays(1, &vertexArray);
+    glBindVertexArray(vertexArray);
+    glCheckError();
+
+    unsigned int vertexBuffer;
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float), positions, GL_STATIC_DRAW);
+    glCheckError();
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+    glCheckError();
+
+    unsigned int indexBuffer;
+    glGenBuffers(1, &indexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 3 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+    glCheckError();
+
+    GLint Result = GL_FALSE;
+    int InfoLogLength;
+
+    unsigned int VertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+    unsigned int FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+    glCheckError();
+
+    char const* VertexSourcePointer = R"glsl(
+    #version 330 core
+
+    layout(location=0) in vec2 position;
+
+    void main()
+    {
+        gl_Position = vec4(position, 0.0, 1.0);
+    }
+    )glsl";
+    glShaderSource(VertexShaderID, 1, &VertexSourcePointer, NULL);
+    glCompileShader(VertexShaderID);
+
+    glGetShaderiv(VertexShaderID, GL_COMPILE_STATUS, &Result);
+    glGetShaderiv(VertexShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+    if (InfoLogLength > 0) {
+        std::vector<char> VertexShaderErrorMessage(InfoLogLength + 1);
+        glGetShaderInfoLog(VertexShaderID, InfoLogLength, NULL, &VertexShaderErrorMessage[0]);
+        std::cout << &VertexShaderErrorMessage[0] << std::endl;
+    }
+
+    char const* FragmentSourcePointer = R"glsl(
+    #version 330 core
+
+    out vec4 color;
+
+    void main()
+    {
+        color = vec4(1.0, 0.0, 0.0, 1.0);
+    }
+    )glsl";
+    
+
+    glShaderSource(FragmentShaderID, 1, &FragmentSourcePointer, NULL);
+    glCompileShader(FragmentShaderID);
+
+    unsigned int shader = glCreateProgram();
+    glAttachShader(shader, VertexShaderID);
+    glAttachShader(shader, FragmentShaderID);
+    glLinkProgram(shader);
+    glCheckError();
+
+    glGetProgramiv(shader, GL_LINK_STATUS, &Result);
+    glGetProgramiv(shader, GL_INFO_LOG_LENGTH, &InfoLogLength);
+    if (InfoLogLength > 0) {
+        std::vector<char> ProgramErrorMessage(InfoLogLength + 1);
+        glGetProgramInfoLog(shader, InfoLogLength, NULL, &ProgramErrorMessage[0]);
+        std::cout << &ProgramErrorMessage[0] << std::endl;
+    }
+    glCheckError();
+
+    glDetachShader(shader, VertexShaderID);
+    glDetachShader(shader, FragmentShaderID);
+    glDeleteShader(VertexShaderID);
+    glDeleteShader(FragmentShaderID);
+    glUseProgram(shader);
+    glCheckError();
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
